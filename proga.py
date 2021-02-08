45b92ea50908c9c2186e14274953c52450d2c9bd
Alexey Koryakin <al9997@mail.ru>
2021-02-08 17:31:58 +0900

    dobavil umnojenie

--- proga.py
+++ proga.py
@@ -4,8 +4,12 @@ def plus(x, y):
 def minus(x, y):
    return x - y
 
+def umnojit(x, y):
+   return x * y
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 print(num1,"+",num2,"=", plus(num1,num2))
 print(num1,"-",num2,"=", minus(num1,num2))
+print(num1,"*",num2,"=", umnojit(num1,num2))
