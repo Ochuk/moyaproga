f13857e54e2497e4522d7a51f6c2e72191b69863
Alexey Koryakin <al9997@mail.ru>
2021-02-08 17:24:50 +0900

    dobavil minus

--- proga.py
+++ proga.py
@@ -1,5 +1,11 @@
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+def plus(x, y):
+   return x + y
+
+def minus(x, y):
+   return x - y
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1,"+",num2,"=", plus(num1,num2))
+print(num1,"-",num2,"=", minus(num1,num2))
