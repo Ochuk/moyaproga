e24700c427512090a31d7f6491337febda832961
Alexey Koryakin <al9997@mail.ru>
2021-02-08 17:34:53 +0900

    dobavil delenie

--- proga.py
+++ proga.py
@@ -7,9 +7,13 @@ def minus(x, y):
 def umnojit(x, y):
    return x * y
 
+def delit(x, y):
+   return x / y
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 print(num1,"+",num2,"=", plus(num1,num2))
 print(num1,"-",num2,"=", minus(num1,num2))
 print(num1,"*",num2,"=", umnojit(num1,num2))
+print(num1,"/",num2,"=", delit(num1,num2))
